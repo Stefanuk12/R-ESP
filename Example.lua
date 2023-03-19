1bad522adeb4109b605f2f92d01fb396e5c1282d
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2023-03-19 22:21:36 +0000

    Moved examples

--- Example.lua
+++
@@ -1,5 +0,0 @@
--- // Dependencies
-local RESP = loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/R-ESP/master/Manager.lua"))().PlayersManager.new()
-
--- // Initialise
-RESP:Initialise()
